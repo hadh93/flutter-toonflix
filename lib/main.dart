e86a09af342c64b54c00a57caed099e4ec78868c
hadh93 <hoondog1993@gmail.com>
2023-02-16 13:46:55 -0600

    3.0 Header

--- lib/main.dart
+++ lib/main.dart
@@ -8,15 +8,44 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Hello Flutter!'),
-        ),
-        body: Center(
-          child: Text('Hello World!'),
+        home: Scaffold(
+      backgroundColor: Color(0xFF181818),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 40),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 80,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Text(
+                      'Hey, Selena',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 28,
+                        fontWeight: FontWeight.w800,
+                      ),
+                    ),
+                    Text(
+                      'Welcome back',
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.white.withOpacity(.8),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
         ),
       ),
-    );
+    ));
     // "root" of our app
     // option: material - Google/ cupertino - iOS
   }
