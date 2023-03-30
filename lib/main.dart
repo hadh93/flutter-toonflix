954de6868180d5738c339dacf714b2a6872084b6
hadh93 <hoondog1993@gmail.com>
2023-03-30 14:21:12 -0500

    #4.3 BuildContext

--- lib/main.dart
+++ lib/main.dart
@@ -18,40 +18,40 @@ class App extends StatefulWidget {
 class _AppState extends State<App> {
   int counter = 0;
 
-  void onClicked() {
-    // setState함수를 호출하지 않으면 'build'메서드가 재실행되지 않아 UI가 변화하지 않음.
-    setState(() {
-      counter = counter + 1;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        textTheme: const TextTheme(
+          titleLarge: TextStyle(
+            color: Colors.red,
+          ),
+        ),
+      ),
       home: Scaffold(
         backgroundColor: const Color(0xFFF4EDDB),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                'Click Count',
-                style: TextStyle(fontSize: 30),
-              ),
-              Text(
-                '$counter',
-                style: const TextStyle(fontSize: 30),
-              ),
-              IconButton(
-                onPressed: onClicked,
-                icon: const Icon(
-                  Icons.add_box_rounded,
-                ),
-              )
-            ],
+            children: const [MyLargeTitle()],
           ),
         ),
       ),
+    );
+  }
+}
+
+class MyLargeTitle extends StatelessWidget {
+  const MyLargeTitle({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'My Large Title',
+      style: TextStyle(
+          fontSize: 30, color: Theme.of(context).textTheme.titleLarge?.color),
     );
   }
 }
